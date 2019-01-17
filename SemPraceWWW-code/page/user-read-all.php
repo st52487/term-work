2f4bc1559c6fcc98ec65b1a040dd878e6950ee23
st52487 <st52487@student.upce.cz>
2019-01-18 00:07:02 +0100

    funkcni update

--- SemPraceWWW-code/page/user-read-all.php
+++ SemPraceWWW-code/page/user-read-all.php
@@ -66,16 +66,16 @@ $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $data = $conn->query("select id_ucitel,jmeno,prijmeni,druhrole,nazev from ucitel join role r on ucitel.id_role = r.id_role join trida t on ucitel.id_trida = t.id_trida;")->fetchAll();
 
 ?>
-<a href="?page=user-add">Přidat uživatele</a>
+<?php if($_SESSION["role"] == 'reditel') {?>
+    <a href="?page=user-add">Přidat uživatele</a>
 
-<?php
-echo '<table align="center" class="blueTable" style="height: 50px;" width="100px">
+    <?php
+    echo '<table align="center" class="blueTable" style="height: 50px;" width="100px">
 ';
 
-echo '  
+    echo '  
   <thead>
 <tr>
-<th>Id</th>
 <th>Jméno</th>
 <th>Přijmení</th>
 <th>Role</th>
@@ -88,21 +88,57 @@ echo '
 </tr>
 </tfoot>';
 
-echo ' <tbody>';
-foreach ($data as $row) {
+    echo ' <tbody>';
+    foreach ($data as $row) {
 
-    echo '   
+        echo '   
    <tr > 
     <td >' . $row["jmeno"] . '</td >
     <td >' . $row["prijmeni"] . '</td > 
     <td >' . $row["druhrole"] . '</td >
     <td >' . $row["nazev"] . '</td >
+    <?php if ($_SESSION["role"] == \'reditel\') { ?>
     <td>
-        <a href="?page=user-update&action=update&id_ucitel='.$row["id_ucitel"].'">U</a>
-        <a href="?page=delete-user&action=delete&id_ucitel='.$row["id_ucitel"].'">D</a>
+        <a href="?page=user-update&action=update&id_ucitel=' . $row["id_ucitel"] . '">U</a>
+        <a href="?page=delete-user&action=delete&id_ucitel=' . $row["id_ucitel"] . '">D</a>
     </td>
+    <?php } ?>
   </tr >';
 
-}
+    }
 
-echo '</tBody></table>';
+    echo '</tBody></table>';
+}else{
+    echo '<table align="center" class="blueTable" style="height: 50px;" width="100px">
+';
+
+    echo '  
+  <thead>
+<tr>
+<th>Jméno</th>
+<th>Přijmení</th>
+<th>Role</th>
+<th>Třída</th>
+</tr>
+</thead>
+<tfoot>
+<tr>
+<td colspan="4">&nbsp;</td>
+</tr>
+</tfoot>';
+
+    echo ' <tbody>';
+    foreach ($data as $row) {
+
+        echo '   
+   <tr > 
+    <td >' . $row["jmeno"] . '</td >
+    <td >' . $row["prijmeni"] . '</td > 
+    <td >' . $row["druhrole"] . '</td >
+    <td >' . $row["nazev"] . '</td >
+  </tr >';
+
+    }
+
+    echo '</tBody></table>';
+}?>
--- SemPraceWWW-code/page/user-update.php
+++ SemPraceWWW-code/page/user-update.php
@@ -2,29 +2,35 @@
 $errorFeedbacks = array();
 $successFeedback = "";
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    //todo more validation rules
-    if (empty($_POST["username"])) {
+    if (empty($_POST["prihlasovacijmeno"])) {
         $feedbackMessage = "username is required";
         array_push($errorFeedbacks, $feedbackMessage);
     }
 
-    if (empty($_POST["password"])) {
+    if (empty($_POST["heslo"])) {
         $feedbackMessage = "password is required";
         array_push($errorFeedbacks, $feedbackMessage);
     }
 
     if (empty($errorFeedbacks)) {
-        //success
         $conn = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASSWORD);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-        $stmt = $conn->prepare("UPDATE users SET email= :email, username= :username, password= :username, description= :description WHERE id= :id");
-        $stmt->bindParam(':id', $_POST["id"]);
-        $stmt->bindParam(':email', $_POST["email"]);
-        $stmt->bindParam(':username', $_POST["username"]);
-        $stmt->bindParam(':password', $_POST["password"]);
-        $stmt->bindParam(':description', $_POST["description"]);
+        $stmt = $conn->prepare("UPDATE users SET prihlasovacijmeno= :prihlJmeno, heslo= :heslo WHERE id_ucitel= :id");
+        $stmt->bindParam(':id', $_GET["id_ucitel"]);
+        $stmt->bindParam(':prihlJmeno', $_POST["prihlasovacijmeno"]);
+        $stmt->bindParam(':heslo', md5($_POST["heslo"]));
         $stmt->execute();
+
+        $stmt = $conn->prepare("UPDATE ucitel SET jmeno= :jmeno, prijmeni= :prijmeni, id_role = :id_role, id_trida = :id_trida WHERE id_ucitel= :id");
+        $stmt->bindParam(':id', $_GET["id_ucitel"]);
+        $stmt->bindParam(':jmeno', $_POST["jmeno"]);
+        $stmt->bindParam(':prijmeni', $_POST["prijmeni"]);
+        $stmt->bindParam(':id_role', $_POST["select"]);
+        $stmt->bindParam(':id_trida', $_POST["selectTrida"]);
+        $stmt->execute();
+
+
         $successFeedback = "User was updated";
     }
 }
@@ -45,29 +51,56 @@ if (!empty($errorFeedbacks)) {
 if (empty($errorFeedbacks)) { //load data origin data from database
     $conn = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASSWORD);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $stmt = $conn->prepare("SELECT * FROM users WHERE id= :id");
-    $stmt->bindParam(':id', $_GET["id"]);
+    $stmt = $conn->prepare("select jmeno,prijmeni,druhrole,prihlasovacijmeno,nazev, t.id_trida,r.id_role from ucitel join trida t on ucitel.id_trida = t.id_trida
+join role r on ucitel.id_role = r.id_role join users u on ucitel.id_ucitel = u.id_ucitel where u.id_ucitel = :id");
+    $stmt->bindParam(':id', $_GET["id_ucitel"]);
     $stmt->execute();
     $user = $stmt->fetch();
 
-    $emailValue = $user["email"];
-    $usernameValue = $user["username"];
-    $passwordValue = $user["password"];
-    $descriptionValue = $user["description"];
-} else { //in case of any error, load data
-    $emailValue = $_POST["email"];
-    $usernameValue = $_POST["username"];
-    $passwordValue = md5($_POST["password"]);
-    $descriptionValue = $_POST["description"];
+    $jmeno = $user["jmeno"];
+    $prijmeni = $user["prijmeni"];
+    $druhRole = $user["druhrole"];
+    $prihlasJmeno = $user["prihlasovacijmeno"];
+    $nazev = $user["nazev"];
 }
 ?>
 
 <form method="post">
-    <input type="hidden" name="id" value="<?= $_GET["id"]; ?>">
-    <input type="email" name="email" placeholder="Your email" value="<?= $emailValue; ?>"/>
-    <input type="text" name="username" placeholder="Your username" value="<?= $usernameValue; ?>">
-    <input type="password" name="password" placeholder="Password" value="<?= $passwordValue; ?>">
-    <label for="description-textarea">Description:</label>
-    <textarea name="description" id="description-textarea"><?= $descriptionValue; ?></textarea>
+    <input type="jmeno" name="jmeno" value="<?= $jmeno; ?>"/>
+    <input type="prijmeni" name="prijmeni" value="<?= $prijmeni; ?>"/>
+    <?php
+    $conn = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASSWORD);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $sql = "SELECT id_role, druhrole FROM role";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    if ($stmt->rowCount() > 0) { ?>
+        <select name="select" id="select">
+
+            <?php foreach ($results as $row) { ?>
+                <option value="<?php echo $row['id_role']; ?>"><?php echo $row['druhrole']; ?></option>
+            <?php } ?>
+        </select>
+    <?php } ?>
+    <input type="prihlasovacijmeno" name="prihlasovacijmeno" value="<?= $prihlasJmeno; ?>"/>
+    <input type="heslo" name="heslo" placeholder="heslo"/>
+    <?php
+    $conn = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASSWORD);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $sql = "SELECT id_trida,nazev FROM trida";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    if ($stmt->rowCount() > 0) { ?>
+        <select name="selectTrida" id="selectTrida">
+
+            <?php foreach ($results as $row) { ?>
+                <option value="<?php echo $row['id_trida']; ?>"><?php echo $row['nazev']; ?></option>
+            <?php } ?>
+        </select>
+    <?php } ?>
     <input type="submit" name="isSubmitted" value="yes">
 </form>
